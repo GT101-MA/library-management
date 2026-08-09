cdf152e62fbc8679db0113874f48d344f9e03968
Gabor Tibor <gabortibor101@gmail.com>
2026-08-09 15:46:51 +0200

    Create first routs

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Zdravo Tibore!';
+});
+
+Route::get('/about', function () {
+    return 'Ovo je about stranica';
+});
+
+Route::get('/kontakt', function () {
+    return 'Kontaktirajte nas!';
+});
+
+Route::get('/service', function () {
+    return 'Naše usluge';
+});
+
+Route::get('/blog', function () {
+    return 'Dobro došli na blog';
 });
