5e30d3674894c8090543b381f2b9c5162f4e771b
Gabor Tibor <gabortibor101@gmail.com>
2026-08-12 15:40:01 +0200

    Configure Book model and database constraints

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
     public function index()
     {
+        Book::create([
+            'title' => 'Clean Code',
+            'author' => 'Robert C. Martin',
+            'publication_year' => 2008,
+            'isbn' => '9780132350884',
+        ]);
+        
         return view('books');
     }
 }
--- database/migrations/2026_08_10_140944_create_books_table.php
+++ database/migrations/2026_08_10_140944_create_books_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('author');
             $table->integer('publication_year');
-            $table->string('isbn')->unique;
+            $table->string('isbn')->unique();
             $table->timestamps();
         });
     }
